d68c1f7a80cb26258a562139637a55e3082b0247
JoaoPedroVisconti <visconti.joao@gmail.com>
2021-06-14 13:53:02 +0100

    Create the Navbar and Footer components

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,14 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
+// Components
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Homepage</h1>
       <p>
         Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
@@ -14,6 +19,7 @@ export default function Home() {
         Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
         ligula eget dolor. Aenean massa.
       </p>
+      <Footer />
     </div>
   );
 }
